d22519c15b55feaaa36a9d2fe5718b8b698460d3
RealOkabe <anmolzagrawal@gmail.com>
2022-01-09 23:17:39 +0530

    Added solution to a8

--- activities/src/bin/a8.rs
+++ activities/src/bin/a8.rs
@@ -9,4 +9,55 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum Flavors {
+    Strawberry,
+    Mango,
+    Chocolate,
+    Coffee,
+    Oreo,
+    Nutella,
+}
+
+struct Drink {
+    flavor: Flavors,
+    quantity: i32,
+}
+
+fn check_drink(drink: Drink) {
+    let mut flavor = "";
+    match drink.flavor {
+        Flavors::Strawberry => flavor = "Strawberry",
+        Flavors::Mango => flavor = "Mango",
+        Flavors::Chocolate => flavor = "Chocolate",
+        Flavors::Coffee => flavor = "Coffee",
+        Flavors::Oreo => flavor = "Oreo",
+        Flavors::Nutella => flavor = "Nutella",
+    }
+    println!("The flavor of the drink is {:?} and its quantity is {:?} ounces", flavor, drink.quantity);
+}
+
+fn main() {
+    let coffee = Drink {
+        flavor: Flavors::Coffee,
+        quantity: 8,
+    };
+    let strawberry = Drink {
+        flavor: Flavors::Strawberry,
+        quantity: 12,
+    };
+    let chocolate = Drink {
+        flavor: Flavors::Chocolate,
+        quantity: 10,
+    };
+    let mango = Drink {
+        flavor: Flavors::Mango,
+        quantity: 12,
+    };
+    let nutella = Drink {
+        flavor: Flavors::Nutella,
+        quantity: 6,
+    };
+    check_drink(coffee);
+    check_drink(mango);
+    check_drink(nutella);
+}
