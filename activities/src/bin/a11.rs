6db3cf4a5b8c86d33471336827310edd32c1fcf0
RealOkabe <anmolzagrawal@gmail.com>
2022-01-11 00:13:36 +0530

    Added solution to a11

--- activities/src/bin/a11.rs
+++ activities/src/bin/a11.rs
@@ -8,5 +8,27 @@
 // * Use two i32 fields for the quantity and id number
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
+// Apparently, the default behavior in rust is to delete the variable after it has been used in a function once unless specified otherwise
+// To make sure that the variable isn't deleted, we have to make the functions borrow the variable instead of the default moving
 
-fn main() {}
+struct Item {
+    id: i32,
+    quantity: i32,
+}
+
+fn check_quantity(item: &Item) {
+    println!("The quantity of the item is {:?}", item.quantity);
+}
+
+fn check_id(item: &Item) {
+    println!("The id of the item is {:?}", item.id);
+}
+
+fn main() {
+    let item = Item {
+        id: 00001,
+        quantity: 10,
+    };
+    check_id(&item);
+    check_quantity(&item);
+}
